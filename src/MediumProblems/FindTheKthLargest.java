20618aedd3c63fa2ff7c88021a331d0d2041425f
Khankee <1940930@brunel.ac.uk>
2022-07-21 14:28:07 +0100

    Medium update

--- src/MediumProblems/FindTheKthLargest.java
+++ src/MediumProblems/FindTheKthLargest.java
@@ -1,21 +1,37 @@
 package MediumProblems;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class FindTheKthLargest {
     public static void main(String[] args) {
 
-        int[] nums = {5,3,7,2,4,1};
-        System.out.println(findKthLargest2(nums,3));
+        int[] nums = {3,2,1,5,6,4};
+        System.out.println(findKthLargest2(nums,2));
     }
 
     public static int findKthLargest(int[] nums, int k){
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int val : nums){
+            heap.add(val);
+        }
+        System.out.println(Arrays.toString(heap.toArray()));
+
+        for (int l = 1; l < k; l++){
+            heap.poll();
+        }
+        return heap.poll();
+    }
+
+    public static int findKthLargest2(int[] nums, int k){
         PriorityQueue<Integer> heap = new PriorityQueue<>();
 
         for (int n : nums){
             heap.add(n);
+            System.out.println(Arrays.toString(heap.toArray()));
             if(heap.size() > k){
                 heap.poll();
             }
@@ -24,7 +40,8 @@ public class FindTheKthLargest {
         return heap.poll();
     }
 
-    public static int findKthLargest2(int[] nums, int k){
+
+    public static int findKthLargest3(int[] nums, int k){
         Arrays.sort(nums);
         return nums[nums.length - k];
     }
