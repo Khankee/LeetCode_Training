50abf7a2d6bf038444ab630b27afe9e959cd2bdd
Khankee <1940930@brunel.ac.uk>
2022-07-21 14:40:47 +0100

    Easy update

--- src/EasyProblems/LOngestPalindrome.java
+++ src/EasyProblems/LOngestPalindrome.java
@@ -6,32 +6,16 @@ public class LOngestPalindrome {
     }
 
     public static int longestPalindrome(String s){
-        if (s.length() == 1) return 1;
-        int[] alphabetArray = new int[52];
-        for (int i = 0; i < s.length(); i++){
-            if(s.charAt(i) > 96){
-                alphabetArray[s.charAt(i) - 'a']++;
-            } else {
-                alphabetArray[s.charAt(i) - 'A' + 26]++;
-            }
-        }
-        int palindromeOdd = 1;
-        int count = 0;
-        for (int i = 0; i < alphabetArray.length; i++){
-            if(alphabetArray[i] > 1){
-                if(alphabetArray[i] % 2 == 0){
-                    count += alphabetArray[i];
-                }
-                else {
-                    count = count + alphabetArray[i] - 1;
-                }
-            }
-            else if(alphabetArray[i] == 1 && palindromeOdd == 1){
-                count++;
-                palindromeOdd -= 1;
-            }
-        }
+        int[] count = new int[128];
+        for (char c: s.toCharArray())
+            count[c]++;
 
-        return count;
+        int ans = 0;
+        for (int v: count) {
+            ans += v / 2 * 2;
+            if (ans % 2 == 0 && v % 2 == 1)
+                ans++;
+        }
+        return ans;
     }
 }
