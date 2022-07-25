d71e37bd1bcf43f212452db9f6f2562471b94ae3
Khankee <1940930@brunel.ac.uk>
2022-07-25 16:31:33 +0100

    learning

--- src/Learning/XOR.java
+++ src/Learning/XOR.java
@@ -3,13 +3,23 @@ package Learning;
 public class XOR {
     public static void main(String[] args) {
         int[] nums = {2,3,3,2,1,1,5};
-        System.out.println(singleNumber(nums));
+        //System.out.println(singleNumber(nums));
+        System.out.println(countOne(20000));
     }
     public static int singleNumber(int[] nums) {
         int result = 0;
         for(int i : nums) {
-            result ^= i;
+                result ^= i;
         }
         return result;
+    }
+    public static int countOne(int n){
+        int count =0;
+        while (count < 10){
+            n = n & (n-1);
+            System.out.println(n);
+            count++;
+        }
+        return count;
     }
 }
