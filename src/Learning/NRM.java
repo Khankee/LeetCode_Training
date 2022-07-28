7d2bcb77439f9e41990b826c9eaa7c5ceb75bd23
Khankee <1940930@brunel.ac.uk>
2022-07-28 14:45:29 +0100

    NRM update

--- src/Learning/NRM.java
+++ src/Learning/NRM.java
@@ -1,14 +1,17 @@
 package Learning;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class NRM {
 
-    static final double EPSILON = 0.001;
+    static final double EPSILON = 1;
 
     // An example function whose solution. is determined using Bisection Method.
     // The function is x^7 - 5 * x^4 + 2 * x - 17
     static double function(double x)
     {
-        return Math.pow(x,7) - (5 * Math.pow(x,4)) + (2 * x) - 17;
+        return x * x * x * x * x * x * x - 5 * x * x * x * x + 2 * x - 17;
     }
 
     // Derivative of the above function
@@ -30,17 +33,30 @@ public class NRM {
             x = x - h;
         }
 
-        System.out.print("The value of the"
+        DecimalFormat df = new DecimalFormat("#.###");
+        System.out.println("The value of the"
                 + " root is : "
-                + Math.round(x * 100.0) / 100.0);
+                + df.format(x));
     }
 
     // Driver code
     public static void main (String[] args)
     {
+        Scanner scan = new Scanner(System.in);
 
-        // Initial values assumed
-        double x0 = 0.5;
-        newtonRaphson(x0);
+        System.out.println(Math.cos(Math.sin(0.5) + 0.5));
+
+        System.out.println("Welcome to my Java Program - Newton Raphson");
+        System.out.println("(i) x ^ 7 - 5 * 4 + 2 * x - 17");
+        System.out.println("Please input the value of x0 between 0 amd 1:");
+
+        while(true){
+            double x0 = scan.nextDouble();
+            if(x0 >= 0 && x0 <= 1){
+                System.out.println("You have input the value of x0 as: " + x0);
+                newtonRaphson(x0);
+            }
+            System.out.println("Please input a value between 0 and 1 inclusive");
+        }
     }
 }
