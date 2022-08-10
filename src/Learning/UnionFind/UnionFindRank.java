cbb6a5ca8c4bb04c95037581f03ad9ed75c3d318
Khankee <1940930@brunel.ac.uk>
2022-08-10 08:38:55 +0100

    Quick union Rank (Disjoint Set)

---
+++ src/Learning/UnionFind/UnionFindRank.java
@@ -0,0 +1,42 @@
+package Learning.UnionFind;
+
+public class UnionFindRank {
+    private int[] root;
+    private int[] rank;
+
+    public UnionFindRank(int size) {
+        root = new int[size];
+        rank = new int[size];
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    public int find(int x) {
+        while (x != root[x]) {
+            x = root[x];
+        }
+        return x;
+    }
+
+    // Union O(log N)
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+        }
+    }
+
+    public boolean connected(int x, int y) {
+        return find(x) == find(y);
+    }
+}
